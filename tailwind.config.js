59a891bc20c5f86636c33a75531f838c80560a5b
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-07-30 17:08:32 +0000

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/workspaces/MileStone-Repo/Project-5/index.html"],
+  content: ["/workspaces/MileStone-Repo/Project-4/*.html","/workspaces/MileStone-Repo/Project-5/*.html"],
   theme: {
     extend: {},
   },
